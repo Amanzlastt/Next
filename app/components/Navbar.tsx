40638aae37325d9836c999343d4cf75fd25887c6
aman123 <amanuelbizuayehu123@gmail.com>
2025-06-23 11:06:32 +0300

    sign and sigh out page devlopment

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { auth, signOut, signIn } from '@/auth';
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className='px-5 py-3 bg-white shadow-md font-work-sans'>
+      <nav className='flex justify-between items-center'>
+        <Link href='/'>
+          <Image src='/logo.png' alt='logo' width={144} height={30} className='text-black' />
+        </Link>
+        <div className='flex items-center gap-5 text-black'>
+          {session?.user ? (
+            <>
+              <Link href='/startup/create'>
+                <span>Create</span>
+              </Link>
+              <button onClick={signOut}>
+                <span >Logout</span>
+              </button>
+              <Link href='/user/${session ?.id}'>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <button onClick={signIn (provider:'github')}>
+                <span>Login</span>
+            </button>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
